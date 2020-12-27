ffa1da296f6f6c436c732f6881f96e4f054ba184
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2020-12-27 00:28:49 -0500

    added segments register

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Simon Says
 
 Progressively creating Simon Says on a DE1-SoC, playable over VGA.
+
+Uses [SystemC/Verilog Random Number Generator](https://opencores.org/projects/systemc_rng), sourced from [here](https://github.com/fabriziotappero/ip-cores.git).
--- simon_says.v
+++ simon_says.v
@@ -36,3 +36,25 @@ module reg8_32(input clk, reset, output reg [31:0] seed);
 			seed[24+i] <= seed[7-i];
 		end
 endmodule
+
+// Stores up to 32 previous colours, with logic "1" msb for unassigned position
+module segments_array(input [2:0] new_colour, input reset, clk, load_colour, output reg [2:0] segment [31:0]);
+	always@(posedge clk)
+	begin
+		if(reset) // All segments are unassigned
+			for(i=0;i<32;i+1)
+			begin
+				segment[i][2] <= 1'b1;
+				segment[i][1] <= 1'b0;
+				segment[i][0] <= 1'b0;
+			end
+		else if(load_colour)
+		begin
+			for(i=0;i<31;i+1)
+				segment[i+1] <= segment[i];
+			segment[0] <= new_colour;
+		end
+		else
+			segment <= segment;
+	end
+endmodule
